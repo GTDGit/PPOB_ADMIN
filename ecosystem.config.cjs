579e75dfe3a06ed353a7113f10bd396e4b6b7415
FlautonGT <rikobudisaputra14@gmail.com>
2026-04-04 05:14:14 +0700

    Use standalone server for PM2 runtime

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,7 @@ module.exports = {
     {
       name: "ppob_admin",
       cwd: "/home/ubuntu/admin",
-      script: "node_modules/next/dist/bin/next",
-      args: "start -p 3001",
+      script: ".next/standalone/server.js",
       env: {
         NODE_ENV: "production",
         PORT: 3001,
